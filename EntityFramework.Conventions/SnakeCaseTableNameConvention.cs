1a3f52f00f569a754061b67e0580e07ebf5a1a36
Aleksander Berus <a.berus@gmail.com>
2017-04-01 10:25:50 +0200

    Add project files

---
+++ EntityFramework.Conventions/SnakeCaseTableNameConvention.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.DependencyResolution;
+using System.Data.Entity.Infrastructure.Pluralization;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EnitityFramework.Conventions
+{
+    public class SnakeCaseTableNameConvention : Convention
+    {
+        public SnakeCaseTableNameConvention()
+        {
+            Types().Configure(c => c.ToTable(GetTableName(c.ClrType)));
+        }
+
+        private string GetTableName(Type type)
+        {
+            var pluralizationService = DbConfiguration.DependencyResolver.GetService<IPluralizationService>();
+
+            var result = pluralizationService.Pluralize(type.Name);
+
+            result = Regex.Replace(result, ".[A-Z]", m => m.Value[0] + "_" + m.Value[1]);
+
+            return result.ToLower();
+        }
+    }
+}
