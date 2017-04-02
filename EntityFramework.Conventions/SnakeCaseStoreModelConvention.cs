addf31cc84a4e4fd28c4acfd3873982cc73d1d4c
Aleksander Berus <a.berus@gmail.com>
2017-04-02 15:35:44 +0200

    Added SnakeCaseStoreModelConvention

---
+++ EntityFramework.Conventions/SnakeCaseStoreModelConvention.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnitityFramework.Conventions
+{
+    public class SnakeCaseStoreModelConvention : IStoreModelConvention<EntitySet>, IStoreModelConvention<EdmProperty>
+    {
+        public void Apply(EntitySet item, DbModel model)
+        {
+            item.Table = SnakeCaseConverter.ConvertToSnakeCase(item.Table);
+        }
+
+        public void Apply(EdmProperty item, DbModel model)
+        {
+            item.Name = SnakeCaseConverter.ConvertToSnakeCase(item.Name);
+        }
+
+    }
+
+}
