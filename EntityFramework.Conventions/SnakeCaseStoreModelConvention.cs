f3176ee1cbadebe8ac2c14bcb7eff26e1213e02e
Aleksander Berus <a.berus@gmail.com>
2017-04-06 22:21:58 +0200

    Added snake case names for Many-to-Many tables

--- EntityFramework.Conventions/SnakeCaseStoreModelConvention.cs
+++ EntityFramework.Conventions/SnakeCaseStoreModelConvention.cs
@@ -10,18 +10,75 @@ using System.Threading.Tasks;
 
 namespace EnitityFramework.Conventions
 {
-    public class SnakeCaseStoreModelConvention : IStoreModelConvention<EntitySet>, IStoreModelConvention<EdmProperty>
+    public class SnakeCaseStoreModelConvention : IStoreModelConvention<EntityType>, IStoreModelConvention<EdmProperty>, IStoreModelConvention<AssociationType>
     {
-        public void Apply(EntitySet item, DbModel model)
+        public void Apply(EntityType item, DbModel model)
         {
-            item.Table = SnakeCaseConverter.ConvertToSnakeCase(item.Table);
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            if (model == null)
+                throw new ArgumentNullException(nameof(model));
+
+            var entitySet = model.StoreModel.Container.EntitySets.SingleOrDefault(es => es.ElementType == GetRootType(item));
+
+            var tableName = item.MetadataProperties.FirstOrDefault(x => x.Name == "TableName");
+
+            if (tableName == null)
+            {
+                entitySet.Table
+                    = Uniquify(model.StoreModel.Container.EntitySets
+                        .Where(es => es.Schema == entitySet.Schema)
+                        .Except(new[] { entitySet })
+                        .Select(n => n.Table)
+                        , SnakeCaseConverter.ConvertToSnakeCase(entitySet.Table));
+
+            }
         }
 
         public void Apply(EdmProperty item, DbModel model)
         {
-            item.Name = SnakeCaseConverter.ConvertToSnakeCase(item.Name);
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            if (model == null)
+                throw new ArgumentNullException(nameof(model));
+
+            var preferredName = (string)item.MetadataProperties.FirstOrDefault(x => x.Name == "PreferredName")?.Value;
+            if (preferredName == item.Name)
+                item.Name = SnakeCaseConverter.ConvertToSnakeCase(item.Name);
         }
 
-    }
+        public string Uniquify(IEnumerable<string> inputStrings, string targetString)
+        {
+            string uniqueString = targetString;
+            int i = 0;
 
+            while (inputStrings.Any(n => string.Equals(n, uniqueString, StringComparison.Ordinal)))
+                uniqueString = targetString + ++i;
+
+            return uniqueString;
+        }
+
+        EntityType GetRootType(EntityType entityType)
+        {
+            if (entityType.BaseType != null)
+                return GetRootType((EntityType)entityType.BaseType);
+            return entityType;
+        }
+
+        public void Apply(AssociationType item, DbModel model)
+        {
+            var associationsSetMappings = model.ConceptualToStoreMapping.AssociationSetMappings;
+
+            foreach (var associationSetMapping in associationsSetMappings)
+            {
+                var associationSetEnds = associationSetMapping.AssociationSet.AssociationSetEnds;
+                associationSetMapping.StoreEntitySet.Table = String.Format("{0}_{1}",
+                    SnakeCaseConverter.ConvertToSnakeCase(associationSetEnds[0].EntitySet.ElementType.Name),
+                    SnakeCaseConverter.ConvertToSnakeCase(associationSetEnds[1].EntitySet.ElementType.Name));
+            }
+
+        }
+    }
 }
